28a48f7e6c3a9bcef352c33be6e45bee82d1ba8a
mariamodat <mariamodat0@gmail.com>
2021-03-07 20:39:52 +0200

    alerts

--- makeup.js
+++ makeup.js
@@ -7,10 +7,12 @@ document.write ('Hello mariam , add items to your cart');
 }
 else if ( name == "Sara") 
 {   
-    alert ( " hey Sara , plz try Again later") ;
-}
-var Brand = prompt ('plz select what brand you want') ;
+    alert ( " hey Sara , answer the next question") ;
+    document.write('<img src=" https://res.cloudinary.com/teepublic/image/private/s--RIPhJ9tW--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:38/co_ffffff,e_outline:inner_fill:38/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/t_watermark_lock/c_limit,f_auto,h_630,q_90,w_630/v1591687454/production/designs/11141140_0.jpg"/>') ;
 
+}
+alert(' for next question enter sephora');
+var Brand = prompt ('plz select what brand you want') ;
 
 
 
@@ -24,13 +26,10 @@ document.write(' <img src = "https://www.styleimagination.com/wp-content/uploads
 
 
 }
-if (name == "Mariam")
+var items = prompt('how many item you want to add to your cart?') ;
+for (var i =0 ; i<items ; i++)
 {
 
-    document.write('<img src=" https://res.cloudinary.com/teepublic/image/private/s--RIPhJ9tW--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:38/co_ffffff,e_outline:inner_fill:38/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/t_watermark_lock/c_limit,f_auto,h_630,q_90,w_630/v1591687454/production/designs/11141140_0.jpg"/>') ;
-
-}
-else {
-document.write('<img src = "https://cdnb.artstation.com/p/assets/images/images/021/018/773/large/eli-cecil-hello-everyone-yellow-01.jpg?1570048381"/>') ;
+document.write (items);
 
 }
