26c1e138f93f1f1f7cfd58545430d29d06cf27ce
mariamodat <mariamodat0@gmail.com>
2021-03-07 18:48:51 +0200

    three

--- makeup.js
+++ makeup.js
@@ -9,7 +9,28 @@ else if ( name == "Sara")
 {   
     alert ( " hey Sara , plz try Again later") ;
 }
-else { alert (" this is not the user plz try again later") ; 
+else { var Brand = prompt ('plz select what brand you want') ;
 
 
 } 
+
+if (Brand == 'sephora')
+{  
+document.write ('<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzdhaemcQ6SclPpQsAefQ3zznPLdnN79Hd9A&usqp=CAU" />');
+}
+else {
+
+document.write(' <img src = "https://www.styleimagination.com/wp-content/uploads/2019/12/Most-Expensive-Makeup-Brands-2020.jpg"/>') ; 
+
+
+}
+if (name == "Mariam")
+{
+
+    document.write('<img src=" https://res.cloudinary.com/teepublic/image/private/s--RIPhJ9tW--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:38/co_ffffff,e_outline:inner_fill:38/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/t_watermark_lock/c_limit,f_auto,h_630,q_90,w_630/v1591687454/production/designs/11141140_0.jpg"/>') ;
+
+}
+else {
+document.write('<img src = "https://cdnb.artstation.com/p/assets/images/images/021/018/773/large/eli-cecil-hello-everyone-yellow-01.jpg?1570048381"/>') ;
+
+}
