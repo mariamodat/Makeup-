d666cb2521ea064bbfd69a1174cb9ea8afe20099
mariamodat <mariamodat0@gmail.com>
2021-03-07 21:15:50 +0200

    loop

--- loop.js
+++ loop.js
@@ -1,11 +1,11 @@
 var makeupType = prompt ('what do you want to purchase ?' ) ; 
 while (makeupType !== 'maskara' && makeupType !== 'eyeliner' )
 { 
-{
+
     var makeupType = prompt('plz enter maskara') ;
  var numOfItems =prompt ('how many of maskara you want to add to your Cart ?') ; 
 
-for ( var i=0 ; i< numOfItems ; i++) 
+for (  i=0 ; i< numOfItems ; i++) 
 {
 document.write(numOfItems) ; 
 document.write('<img src ="https://st.depositphotos.com/1441511/4328/i/600/depositphotos_43287677-stock-photo-woman-with-beautiful-makeup-and.jpg"/>');
@@ -21,7 +21,7 @@ document.write('sorry items does not match') ;
 
 
 
-}
+
 
 
 
