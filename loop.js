cbcad8c55ed905e3d251e4f832c8f7a6d5943fee
mariamodat <mariamodat0@gmail.com>
2021-03-08 14:33:27 +0200

    functions and css

--- loop.js
+++ loop.js
@@ -1,18 +1,35 @@
-var makeupType = prompt ('what do you want to purchase ?' ) ; 
-while (makeupType == 'lipstick' )
-{ 
-var type =prompt ('plz enter maskara or eyeliner' );
-if (type == 'maskara'  || type == 'eyeliner')
-{
- var numOfItems =prompt ('how many of maskara you want to add to your Cart ?') ;
- var result = numOfItems ;
-for ( i=0 ; i< 5 ; i++)
-{
-document.write(result) ; 
-var order = '<img src " maskara" /'> ;
+ var makeupType = prompt ('what do you want to purchase ?' ) ; 
+
+function entertype () { makeupType =prompt ('plz enter maskara or eyeliner' );
+
+}
+
+
+
+function loop () { var numOfItems =prompt ('how many of maskara you want to add to your Cart ?') ;
+    for ( var i=0 ; i< numOfItems ; i++)
+    {
+    document.write(numOfItems) ; 
+    var order = '<img src =" https://st.depositphotos.com/1441511/4328/i/600/depositphotos_43287677-stock-photo-woman-with-beautiful-makeup-and.jpg" />' ;
+    document.write (order) ;
+    
+    }
 
 
 }
+
+
+
+
+
+ 
+while (makeupType !== 'eyeliner' && makeupType !=="maskara" )
+{  entertype ();
+
+if (makeupType == 'maskara'  || makeupType == 'eyeliner')
+{
+ 
+loop () ; 
 
 }
 else {
